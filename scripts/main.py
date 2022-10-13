7655d02d17c37d55e912caf76a01fc701e1136d7
Muhammad Labiyb Afakh <labiybafakh@gmail.com>
2022-10-13 21:15:58 +0900

    fix ornibibot message

--- scripts/main.py
+++ scripts/main.py
@@ -2,6 +2,7 @@ from enum import auto
 import rospy
 from geometry_msgs.msg import WrenchStamped
 from std_msgs.msg import Int16
+from ornibibot.msg import OrnibiBotData
 import numpy as np
 
 
@@ -9,14 +10,15 @@ class OrnibiBot:
     force = np.zeros(3, dtype=np.float64)
     moment = np.zeros(3, dtype=np.float64)
     wing_position={
-        "left":np.zeros(1),
-        "right":np.zeros(1)
+        "left": 0,
+        "right": 0
     }
 
     def __init__(self) -> None:
         rospy.Subscriber('wing_left', Int16, self.wing_left_callback)
         rospy.Subscriber('wing_right', Int16, self.wing_right_callback)
         rospy.Subscriber('leptrino_force_torque/force_torque', WrenchStamped, self.force_callback)
+        self.stream_data = rospy.Publisher('ornibibot_restream', OrnibiBotData, queue_size=100)
 
     def wing_left_callback(self, position):
         r = self.wing_position["left"] = position.data
@@ -37,5 +39,22 @@ if __name__ == '__main__':
     rospy.init_node('OrnibiBot', anonymous=False)
     robot = OrnibiBot()
 
-    while True:
+    current_time = rospy.Time.now()
+    last_time = rospy.Time.now()
+    while not rospy.is_shutdown():
+        
+
+        ornibibot_data = OrnibiBotData()
+        ornibibot_data.header.stamp = rospy.Time.now()
+        ornibibot_data.header.frame_id = 'ornibibot'
+        ornibibot_data.force.x = robot.force[0]
+        ornibibot_data.force.y = robot.force[1]
+        ornibibot_data.force.z = robot.force[2]
+        ornibibot_data.moment.x = robot.moment[0]
+        ornibibot_data.moment.y = robot.moment[1]
+        ornibibot_data.moment.z = robot.moment[2]
+        ornibibot_data.wing_left.data = int(robot.wing_position["left"])
+        ornibibot_data.wing_right.data = int(robot.wing_position["right"])
+
+        robot.stream_data.publish(ornibibot_data)
         rospy.Rate(100).sleep()
